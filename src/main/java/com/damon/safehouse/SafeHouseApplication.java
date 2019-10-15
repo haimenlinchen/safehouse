95c100b6feccc660497cbbeb82684ccf663e752e
chelin2@cisco.com <52l-C142857>
2019-10-15 16:56:56 +0800

    Initial commit

---
+++ src/main/java/com/damon/safehouse/SafeHouseApplication.java
@@ -0,0 +1,13 @@
+package com.damon.safehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/damon/safehouse/SafehouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.damon.safehouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SafehouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
